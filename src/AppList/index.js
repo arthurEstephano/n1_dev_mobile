fa1fed321dc47ad9a066fb415e728ac5ced9cde6
Arthur Estephano Lemos de Moura <arthurlemos98@hotmail.com>
2023-04-16 16:21:07 -0300

    Creating blank version of app

---
+++ src/AppList/index.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function AppList() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    flex: 1,
+    color: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  }
+});
